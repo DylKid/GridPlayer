217bbf622a9a45e38f037ad7be733f48f726caa5
Dylan Kidd <dylan_kidd@hotmail.com>
2018-10-31 14:10:42 +1000

    Initial commit

---
+++ src/gridPlayer/StateHandler.java
@@ -0,0 +1,35 @@
+package gridPlayer;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class StateHandler {
+
+    private StateObserver observer;
+
+    public void setObserver(StateObserver observer){
+        System.out.println("SETTING OBSERVER");
+        this.observer = observer;
+    }
+
+    public boolean isObserverSet(){
+        if(observer!=null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public abstract Location getCurrentLoc();
+
+    public abstract Location getCurrentGoal();
+
+    public abstract Map<Location, List<Location>> getValidMoveList();
+
+    public void updateNotify(){
+        observer.runNotify();
+    }
+
+    public void updateSuccess() {observer.notifySuccess(); }
+
+}
